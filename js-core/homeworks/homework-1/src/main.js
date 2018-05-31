6460e9837674643c7d69ebc022efcd3907471be8
Nataliia <n.viskosa@gmail.com>
2018-05-31 19:03:19 +0300

    homework_1

--- js-core/homeworks/homework-1/src/main.js
+++ js-core/homeworks/homework-1/src/main.js
@@ -4,7 +4,7 @@ let name = 'Oleksandr',
 	age = 21,
 	phoneNumbers = {
 		kyivstar: 12345678,
-		mts: 87654321
+		mts: 8765432
 	},
 	telegram = +'four seven seven six zero',
 	viber = null,		//тут хотелось бы не напрямую его задать
