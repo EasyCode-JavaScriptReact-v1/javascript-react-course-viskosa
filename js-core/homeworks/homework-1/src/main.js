c9502c4fae59d1d08b71692c907b2552c64c60c0
Nataliia <n.viskosa@gmail.com>
2018-05-31 18:51:50 +0300

    homework_1

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,24 @@
+let name = 'Oleksandr',
+	lastname = 'Polischuk',
+	avatar = true,
+	age = 21,
+	phoneNumbers = {
+		kyivstar: 12345678,
+		mts: 87654321
+	},
+	telegram = +'four seven seven six zero',
+	viber = null,		//тут хотелось бы не напрямую его задать
+	skype;
+
+let user = {
+	name: name,
+	lastmame: lastname,
+	avatar: avatar,
+	age: age,
+	phoneNumbers: phoneNumbers,
+	skype: skype,
+	telegram: telegram,
+	viber: viber
+};
+
+console.log(user);
