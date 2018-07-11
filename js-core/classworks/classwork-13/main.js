078aa7748711947f096df6048606e3cfac5ae2e3
Nataliia <n.viskosa@gmail.com>
2018-07-11 19:51:18 +0300

    hw_13 was partially done

--- js-core/classworks/classwork-13/main.js
+++ js-core/classworks/classwork-13/main.js
@@ -1,5 +1,40 @@
-//ПОЛИМОРФИЗМ-----------------------------------------------
+//---- РАЗБОР ДЗ -----------------------------------
 
+// @SUPER
+/*
+ * TASK 0
+ * Создайте функцию обертку над другой функцией
+ * Каждый раз при вызове внутренней функции в консоле будут отображаться аргументы функции
+*/
+
+const adding = (a, b) => a + b;
+
+const wrapperFunc = (fn) => {
+	return (...args) => {  //из пришедших ыргументов сделает массив
+		console.log(args);
+		return fn(...args);
+		//fn.apply(null, args);
+	}
+}
+
+const result = wrapperFunc(adding);
+console.log(result(10, 20)); //30
+
+
+/*можно вот так упростить:*/
+const wrapperFunc2 = fn => (...args) => {  //из пришедших ыргументов сделает массив
+		console.log(args);
+		return fn(...args);
+	}
+
+const result2 = wrapperFunc(adding);
+console.log(result2(30, 40)); //30
+
+
+
+//ПОЛИМОРФИЗМ-----------------------------------------------
+//Полиморфизм - переопределение в дочернем классе метода родительского класса.
+//Этот переопределенный метод работает только для дочернего класса, в классе-конструкторе остается старый
 //добавьте всем массивам метод sum, который суммирует все аргументы в массиве
 
 const arr = [1,2,3];
@@ -17,59 +52,128 @@ console.log(Array.prototype);
 console.log(arr.sum())//6
 console.log(arr2.sum())//100
 
-//-------------------------------------------------------
+//------ КЛАССЫ -----------------------------------------------
+//----- так мы писали раньше на прототипах: -------------------
 
-function Country (countryName){
+function Country (countryName){			//это ф-ция конструктор
 	this.countryName = countryName;
 }
 
-Country.prototype.showCity = function(){
+Country.prototype.showCity = function(){	// это ее метод, записанный в прототип
 	console.log(this.countryName);
+	console.log('это вызвался оригинальный метод конструктора, метод называется showCity');
 }
 
-const ukraine = new Country('ukraine');
+	function City(city){ 		// еще один конструктор, который будет наследоваться от Country
+		Country.apply(this, city); //наследуемся от Country
+		this.cityName = city;
+	}
 
-ukraine.prototype = Object.create(Country.prototype);
+	City.prototype = Object.create(Country.prototype); //наследуемся от Country
+	City.prototype.showCity = function(){				// это ПОЛИМОРФИЗМ - переопределили поведение метода showCity, 
+		console.log('это вызвался переопределенный метод showCity из дочернего класса'); // взятого из конструктора
+		//теперь все, кто будет наследоваться от City, получат метод showCity такой, как в City, а не как в Country
+	}
+	//----
+	const ukraine = new Country('ukraine'); //строим новый экземпляр ф-ции конструктора
+	console.log('ukraine with constructor-->', ukraine);
 
-ukraine.prototype.showCity = function(){
-	console.log('переопределили');
-}
+	//ukraine.prototype = Object.create(Country.prototype);
 
-//------------------------------------------------------------
+	//ukraine.prototype.showCity = function(){
+	//	console.log('переопределили');
+	//}
+
+//------ теперь так это выглядит на классах ---------------------
 
 class CountryClass {  //ф-ция конструктор
-	constructor(countryName){
+	constructor(countryName){ 			//в constructor приходят аргументы, как раньше они приходили в ф-цию конструктор
 		this.countryName = countryName;
-		let secretPlase = 'Island';
-		this.showSecretPlase = () => secretPlase;
+		let secretPlaсe = 'Island';				  //приватное свойство, пишем прямо в конструктор
+		this.showSecretPlaсe = () => secretPlaсe; //приватный метод, пишем прямо в конструктор
 	}
 
-	showCity() {  //метод
-	console.log(this.countryName);
+	showCity() {  //метод, пишется прям в классе
+		console.log(this.countryName);
 	}
 
-}
-
-const siria = new Country('siria');
-
-//---------------------------------------------------------------
-
-class CityClass extends CountryClass { //наследование
-	constructor(options){
-		super(options.name);
-		this.cityName = options.age;
-	}
-
-	showTree(){
-		console.log('qqqqqqqqqqqqqqqqqqq');
+	showSomethingElse() {  //метод, пишется прям в классе
+		console.log('showSomethingElse');
 	}
 
 }
+//CountryClass.prototype.someMethod = function(){}// в принципе и так можно добавлять методы в класс, но так никто не делает
 
-let city = new CityClass ({name: 'kharkiv', age: 18});
-console.log(city);
+const ukraine2 = new CountryClass('ukraine2'); //строим новый экземпляр класса - все так же точно
+//он унаследует метод showCity через прототипы, т.е. будет лежать в prototype этого экземпляра класса, 
+//все так же, как и с ф-цией конструктором
+console.log('ukraine2 with class-->', ukraine2);
 
-//----------------------------------------------------------------
+//---------- наследование на классах -----------------------------
+
+class CityClass extends CountryClass { //это все равно, как раньше мы писали City.prototype = Object.create(Country.prototype)
+	constructor(options){ // сюда придет kharkiv
+		super('yo-ho-ho'); //то, что передаем в супер, попадает в родительский класс и там приходит в constructor
+							// и родительский класс вызывается с переданными отсюда параметрами
+		//this.cityName = city; // сюда придет kharkiv
+	}
+
+	showCityName(){
+		console.log(this.cityName);
+		//super.showCity(); прям отсюда можно вызвать метод родительского класса
+	}
+
+	//showCity() {  // полиморфизм - так перезапишем метод родительского класса
+	//	console.log('перезаписываем стандартный метод класса showCity');
+	//}
+
+}
+
+const kharkiv = new CityClass('kharkiv');
+
+//let city = new CityClass ({name: 'kharkiv', age: 18});
+//console.log(city);
+
+
+//------ ДЕСТРУКТУРИЗАЦИЯ -------------------------------------
+
+
+let showObj = obj => {
+	console.log(obj.name);
+	console.log(obj.surname);
+}
+
+showObj({name: 'Ivan', surname: 'Ivanov'})//Ivan Ivanov
+
+/*можем упростить:----------------------------*/
+let showObj2 = obj => {
+	let {name, surname} = obj;
+
+	console.log(name);
+	console.log(surname);
+}
+
+showObj2({name: 'Ivan', surname: 'Ivanov'})//Ivan Ivanov
+
+/*и еще лучше----------------------------------*/
+
+let showObj3 = ({name, surname}) => {
+	console.log(name);
+	console.log(surname);
+}
+
+showObj3({name: 'Ivan', surname: 'Ivanov'})//Ivan Ivanov
+
+/* и наконец ...rest  ---------------------------*/
+
+let showObj4 = ({name, ...rest}) => { //возьми из пришедшего объекта только name, а остальные свойства собери в объект
+	console.log(name); //тут только name из пришедшего объекта - Ivan
+	console.log(rest); //а тут {name: 'Ivan', surname: 'Ivanov'}
+}
+
+showObj4({name: 'Ivan', surname: 'Ivanov', age: 25})
+
+//--------ЗАДАЧКИ--------------------------------------------------------
 /*
 Сделайте класс анимал с 4 лапами
 Добавтьте метод roar
@@ -114,43 +218,53 @@ myCat.canDo();
 console.log(myCat.roar());
 
 
-//--------SETTIMEOUT-------------------------
+//--------SETTIMEOUT----------------------------------------
+/*Сначала строки без сеттаймаута попадают в основную очередь
+строки с сеттаймаутом попадают в отдельную очередь
+когда очищается основная очередь, из отдельной в основную попадают сеттаймауты
+и из сеттаймаутов выполнятся сначала те, у которых меньше задержка, 
+даже если они были написаны позже по коду*/
+
+
 const add = (a, b) => console.log(a + b);
 
-add(1, 2);
+add(1, 2);  		//это покажется в консоли первым
 
-setTimeout(() => {
+setTimeout(() => { 	//это покажется в консоли четвертым
 	add(100, 200);
 }, 1000);
 
+setTimeout(() => { 	//это покажется в консоли пятым
+	add(10000, 20000);
+}, 1000);
 
-add(1000, 2000);
+setTimeout(() => { 	//это покажется в консоли третьим
+	add(5, 6);
+}, 500);
 
-///-----------------------------------------
+add(1000, 2000); 	//это покажется в консоли вторым
+
+//----------------------------------------------------------
 let func = function(name){
 	console.log(name);
 } 
 
-setTimeout(() => {
+setTimeout(() => { 	//ЭТОТ ПОКАЖЕТСЯ В КОНСОЛИ ТРЕТЬИМ
 	func('Nataly 1');
 	//console.log('Nataly 1');
 }, 3000);
 
-setTimeout(() => {
+setTimeout(() => { 	//ЭТОТ ПОКАЖЕТСЯ В КОНСОЛИ ВТОРЫМ
 	func('Nataly 2');
 	//console.log('Nataly 2');
 }, 2000);
 
-setTimeout(() => {
+setTimeout(() => {	//ЭТОТ ПОКАЖЕТСЯ В КОНСОЛИ ПЕРВЫЙ
 	func('Nataly 3');
 	//console.log('Nataly 3');
 }, 1000);
 
-/*let myName = setTimeout((name) => {
-	console.log(name);
-}, 1000)
-
-myName('Nataliia');*/
+//---------SETINTERVAL & CLEARINTERVAL-----------------------
 
 let int = setInterval(()=>{
 	func('interval');
@@ -162,7 +276,8 @@ setTimeout(()=>{
 
 //давайте счетчик +1 в консоли
 
-let i = 0;
+/*let i = 0;
 setInterval(()=>{
 	console.log(1);
-}, 1000)
+}, 1000)*/
+
---
+++ js-core/homeworks/homework-13/main.js
@@ -0,0 +1,193 @@
+//'use strict';
+
+/*
+ * TASK - 2
+ *
+ * Перепишите Homework 12 - TASK 1 используя class
+ *
+ * */
+
+//-----------------------------------------------------------------
+// TASK 1
+// Создать класс Human, у которого будут свойства обычного человека:
+// имя, возраст, пол, рост, вес.
+// Используя прототипное наследование создать дочерние классы Worker
+// (дописать в них поля места работы, зарплата, метод "работать")
+// и Student (дописать поля места учебы, стипендией, метод "смотреть сериалы")
+//
+// Создать несколько экземпляров классов Worker и Student, вывести их в консоль.
+// Убедиться что они имеют поля родительского класса Human
+
+ // ---- родительский класс ----
+/*function Human(options){
+	let {name, age, sex, height, weight} = options;
+
+	this.name = name;
+	this. age = age;
+	this.sex = sex;
+	this.height = height;
+	this.weight = weight;
+} */
+class Human {
+	constructor(options){
+		let {name, age, sex, height, weight} = options;
+
+		this.name = name;
+		this. age = age;
+		this.sex = sex;
+		this.height = height;
+		this.weight = weight;
+	}
+}
+
+//--------------класс-наследник worker-----------------------
+/*function Worker(options){	//сюда пришел объект переданных свойств
+	let {job, salary, ...humanProps} = options;
+
+	Human.call(this, humanProps);//call, а не apply потому что humanProps - это объект, а не массив
+
+	this.job = job;
+	this.salary = salary;
+}*/
+
+/*Worker.prototype = Object.create(Human.prototype);
+
+Worker.prototype.working = function(){
+	console.log(this.name + ' can work');
+}*/
+
+class Worker extends Human {
+	constructor({job, salary, ...rest}){ //так писать нормально или лучше сюда принять options, а ниже деструктурировать?
+		super(rest);
+
+		this.job = job;
+		this.salary = salary;
+	}
+
+	working(){
+		console.log(this.name + ' can work');
+	}
+
+}
+
+//--------------student-----------------------
+
+/*function Student(options){
+	let {univer, scolarship, ...rest} = options;
+
+	Human.call(this, rest);
+
+	this.univer = univer;
+	this.scolarship = scolarship;
+}
+
+Student.prototype = Object.create(Human.prototype);
+
+Student.prototype.toSeeMovies = function(){
+	console.log(this.name + ' can see movies');
+}*/
+
+class Student extends Human {
+	constructor({univer, scolarship, ...rest}){
+		super(rest);
+	}
+
+	toSeeMovies() {
+		console.log(this.name + ' can see movies');
+	}
+}
+
+let workerAndrew = new Worker({name:'Andrew', age:23, sex:'male', height:186, weight:80, job: 'pilot', salary: 10000});
+console.log(workerAndrew);
+workerAndrew.working();
+
+
+let studentAlla = new Student({sex:'female', height:160, name:'Alla', age:19, weight:55, univer: 'KIMO', scolarship: false});
+console.log(studentAlla);
+studentAlla.toSeeMovies();
+
+/*
+ * Вы должны создать имитацию медленной базы данных.
+ * TASK - 1 Сделайте Класс Database с методами
+ *
+ *  query
+ *
+ *  При запуске метода query запустите внутренний таймаут, который будет длиться 5 секунд.
+ *  При поступлении еще 1 запроса(если вызвать метод еще раз),
+ *  таймаут должен стартануть сначала
+ *  и ответ должен прийти снова через 5 секунд
+ *
+ * */
+
+class DataBase {
+	constructor(){
+		this.counter = 5;
+	}
+
+	counterFunc() {
+		return this.counter--;
+	}
+
+	query() {
+
+		setTimeout(() => {
+
+			let now = this.counterFunc();
+			if ( now > 0) {
+				console.log(now);
+				this.query();
+				return;
+			}
+			console.log('The web server is down');
+
+		}, 1000);
+
+	}	
+	
+
+}
+
+const dataBase = new DataBase();
+console.log(dataBase);
+dataBase.query(); //при вызове второй раз метода dataBase.query() this.counter уже равен 0 и как его обновить?
+
+//console.log(dataBase.counterFunc());
+//console.log(dataBase.counterFunc());
+//console.log(dataBase.counterFunc());
+
+
+/*function QQQ(){ //closure
+	let counter = 0;
+	this.some = function(){
+		return counter++;
+	}
+}
+
+let www = new QQQ();
+console.log(www.some());
+console.log(www.some());
+console.log(www.some());*/
+
+
+// // 5
+// // 4
+// // 3
+// // 2
+// // 1
+// // console.log('The web server is down') https://www.youtube.com/watch?v=W8_Kfjo3VjU
+
+// dataBase.query();
+// // 5
+// // 4
+// dataBase.query();
+// // 5
+// // 4
+// // 3
+// // 2
+// dataBase.query();
+// 5
+// 4
+// 3
+// 2
+// 1
+// console.log('The web server is down')
