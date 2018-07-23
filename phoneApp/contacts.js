84f468d26a340da5e4aa4cc7976e08f57ef14a08
Nataliia <n.viskosa@gmail.com>
2018-07-23 12:48:26 +0300

    footer was make like a static content

--- phoneApp/contacts.js
+++ phoneApp/contacts.js
@@ -328,38 +328,6 @@ class ContactsPage2 {
         email: 'ViktorKriv@ec.ua'
       }
     ];
-    this.footerContent = [
-      {
-        classImg: 'search',
-        text: 'Contacts',
-        href: 'index.html',
-        additionalClass: 'active'
-      },
-        {
-        classImg: 'th',
-        text: 'Keypad',
-        href: 'keypad.html',
-        additionalClass: ''
-      },
-        {
-        classImg: 'pencil',
-        text: 'Edit contact',
-        href: 'edit-contact.html',
-        additionalClass: ''
-      },
-        {
-        classImg: 'user',
-        text: 'User',
-        href: 'user.html',
-        additionalClass: ''
-      },
-        {
-        classImg: 'plus',
-        text: 'Add user',
-        href: 'add-user.html',
-        additionalClass: ''
-      }
-    ];
 
     this.render();
   }
@@ -425,23 +393,32 @@ class ContactsPage2 {
   }
 
   renderFooter(){
-    let links = this.createLinks(this.footerContent)
     return `<footer class="footer">
               <div class="container bottom-radius">
                 <nav class="main-nav">
-                  ${links}
+                  <a href="index.html" class="tab active">
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                    <span class = "tab-text">Contacts</span>
+                  </a>
+                  <a href="keypad.html" class="tab">
+                    <span class="glyphicon glyphicon-th" aria-hidden="true"></span>
+                    <span class = "tab-text">Keypad</span>
+                  </a>
+                  <a href="edit-contact.html" class="tab">
+                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                    <span class = "tab-text">Edit contact</span>
+                  </a>
+                  <a href="user.html" class="tab">
+                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                    <span class = "tab-text">User</span>
+                  </a>
+                  <a href="add-user.html" class="tab">
+                    <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                    <span class = "tab-text">Add user</span>
+                  </a>
                 </nav>
               </div>
             </footer>`
-  }
-
-  createLinks(arr){
-    return arr.map(item => {
-      return `<a href="${item.href}" class="tab ${item.additionalClass}">
-                  <span class="glyphicon glyphicon-${item.classImg}" aria-hidden="true"></span>
-                  <span class="tab-text">${item.text}</span>
-              </a>`;
-    }).join('');
   }
 
   render(){
